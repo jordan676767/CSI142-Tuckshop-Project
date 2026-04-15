415e8072c8253df7665f0d107f0d1625795cfd3e
amonmwanza105-ops <amonmwanza105@gmail.com>
2026-04-15 17:39:25 +0200

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Menu {
+    public void displayMenu() {
+        System.out.println("\n=== Tuckshop Menu ===");
+        System.out.println("1. Add Product");
+        System.out.println("2. View Products");
+        System.out.println("0. Exit");
+    }
+}
